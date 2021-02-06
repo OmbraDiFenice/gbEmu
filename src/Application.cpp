6086903693f83dae45df1bc470b08b923090030a
Stefano <stefano.stoduto@gmail.com>
2021-02-06 21:52:28 +0100

    test code

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 #include <glad/gl.h>
+#include <core/emu/Video.h>
+#include <core/ui/GLShader.h>
+#include <core/ui/GLProgram.h>
 
 void Application::run() {
     LOG_DBG("start app");
@@ -11,36 +14,109 @@ void Application::run() {
 
     window->setEventCallback(BIND_FN(Application::onEvent));
 
-    while (keepRunning()) {
+    // clang-format off
+    float vertices[4 * 5] = {
+        -0.5f, -0.5f,  0.0f, 0.0f, 0.0f,
+         0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+         0.5f,  0.5f,  0.0f, 1.0f, 1.0f,
+        -0.5f,  0.5f,  0.0f, 0.0f, 1.0f
+    };
+    // clang-format on
 
+    unsigned int vertexArray;
+    glGenVertexArrays(1, &vertexArray);
+
+    unsigned int vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+
+    unsigned int indices[6] = {0, 1, 2, 2, 3, 0};
+    unsigned int indexBuffer;
+    glGenBuffers(1, &indexBuffer);
+
+    // clang-format off
+    std::string vertexSrc = R"(
+        #version 330 core
+
+        layout(location = 0) in vec3 a_Position;
+        layout(location = 1) in vec2 a_TexCoord;
+
+        out vec3 v_Position;
+        out vec2 v_TexCoord;
+
+        void main()
+        {
+            v_TexCoord = a_TexCoord;
+            v_Position = a_Position;
+            gl_Position = vec4(a_Position, 1.0);
+        }
+    )";
+
+    std::string fragmentSrc = R"(
+        #version 330 core
+
+        layout(location = 0) out vec4 color;
+
+        in vec3 v_Position;
+        in vec2 v_TexCoord;
+
+        uniform sampler2D u_Texture;
+
+        void main()
+        {
+            color = texture(u_Texture, v_TexCoord);
+            //color = vec4(v_Position + 0.5, 1.0);
+        }
+    )";
+    // clang-format on
+
+    GLProgram program;
+    program.addShader(vertexSrc, GL_VERTEX_SHADER);
+    program.addShader(fragmentSrc, GL_FRAGMENT_SHADER);
+    program.link();
+
+    // clang-format off
+    unsigned char testTile[] = {
+        0, 3, 3, 3, 3, 3, 0, 0,
+        2, 2, 0, 0, 0, 2, 2, 0,
+        1, 1, 0, 0, 0, 1, 1, 0,
+        2, 2, 2, 2, 2, 2, 2, 0,
+        3, 3, 0, 0, 0, 3, 3, 0,
+        2, 2, 0, 0, 0, 2, 2, 0,
+        1, 1, 0, 0, 0, 1, 1, 0,
+        0, 0, 0, 0, 0, 0, 0, 0
+    };
+    // clang-format on
+    unsigned char tileMap[Video::kTileDataTableSize * Video::kDecodedTileSize];
+    memcpy(&tileMap[0], testTile, Video::kDecodedTileSize);
+    unsigned int textureId;
+    const unsigned int WIDTH = Video::kTileDataTableSize * 8;
+    const unsigned int HEIGHT = 8;
+
+    glCreateTextures(GL_TEXTURE_2D, 1, &textureId);
+    glTextureStorage2D(textureId, 1, GL_RGBA8, WIDTH, HEIGHT);
+
+    glTextureParameteri(textureId, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTextureParameteri(textureId, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTextureSubImage2D(textureId, 0, 0, 0, WIDTH, HEIGHT, GL_RED, GL_UNSIGNED_BYTE, tileMap);
+
+    GLint location = glGetUniformLocation(textureId, "u_Texture");
+    glUniform1i(location, 0);
+
+    while (keepRunning()) {
         // data setup
-        unsigned int vertexArray;
-        glGenVertexArrays(1, &vertexArray);
+        program.bind();
+
         glBindVertexArray(vertexArray);
 
-        unsigned int vertexBuffer;
-        glGenBuffers(1, &vertexBuffer);
         glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-
-        // clang-format off
-        float vertices[4 * 3] = {
-            -0.5f, -0.5f,  0.0f,
-            0.5f, -0.5f,  0.0f,
-            0.5f,  0.5f,  0.0f,
-            -0.5f,  0.5f,  0.0f
-        };
-        // clang-format on
-
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
         glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), nullptr);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (const void*)(3*sizeof(float)));
 
-        unsigned int indexBuffer;
-        glGenBuffers(1, &indexBuffer);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-        unsigned int indices[6] = {0, 1, 2, 2, 3, 0};
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
                      GL_STATIC_DRAW);
 
@@ -48,7 +124,6 @@ void Application::run() {
         glClearColor(0.15f, 0.15f, 0.15f, 1);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // glBindVertexArray(id); // bind updated vertex array
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         for (auto c : _components) {
